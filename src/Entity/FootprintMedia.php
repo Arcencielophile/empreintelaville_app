4dfbfdfb7448ba20ee2731628b7e745bd838d890
idci <jeje9769@live.fr>
2018-05-09 15:49:39 +0200

    Changed the Annotation mapping to XML, API not working

--- src/Entity/FootprintMedia.php
+++ src/Entity/FootprintMedia.php
@@ -6,95 +6,44 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
-/**
- * @ApiResource
- * @ORM\Entity
- */
 class FootprintMedia
 {
-    /**
-     * @var int The entity Id
-     *
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
+
     private $id;
 
-    /**
-     * @var string The media name
-     *
-     * @ORM\Column(type="string")
-     * @Assert\NotBlank
-     */
     protected $name;
 
-    /**
-     * @ORM\ManyToOne(targetEntity="Footprint", inversedBy="media")
-     */
     protected $footprint;
 
-    /**
-     * To string
-     */
     public function __toString()
     {
         return $this->getName();
     }
 
-    /**
-     * Get id.
-     *
-     * @return int
-     */
-    public function getId()
+    public function getId(): ?int
     {
         return $this->id;
     }
 
-    /**
-     * Set name.
-     *
-     * @param string $name
-     *
-     * @return FootprintMedia
-     */
-    public function setName($name)
+    public function setName(string $name): self
     {
         $this->name = $name;
 
         return $this;
     }
 
-    /**
-     * Get name.
-     *
-     * @return string
-     */
-    public function getName()
+    public function getName(): ?string
     {
         return $this->name;
     }
 
-    /**
-     * Set footprint.
-     *
-     * @param \App\Entity\Footprint|null $footprint
-     *
-     * @return FootprintMedia
-     */
-    public function setFootprint(\App\Entity\Footprint $footprint = null)
+    public function setFootprint(\App\Entity\Footprint $footprint = null): self
     {
         $this->footprint = $footprint;
 
         return $this;
     }
 
-    /**
-     * Get footprint.
-     *
-     * @return \App\Entity\Footprint|null
-     */
     public function getFootprint()
     {
         return $this->footprint;
